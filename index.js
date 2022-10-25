08a905b303b53ec634a1435ff37f6c1246d2b050
plomachi <paulo.m.gomes@outlook.com>
2022-10-24 21:31:57 -0300

    divisao tarefas

---
+++ index.js
@@ -0,0 +1,7 @@
+// Arieli - Responsável por criar a função de listar todos os usuários acima de 18 anos.
+
+// Silvio - esponsável por criar a função de listar todos os usuários inserindo mais uma informação para cada de ativo: true
+
+// Paulo - Responsável por criar a função de adicionar um usuário
+
+// ALL - Responsável por criar a função de remover um usuário
