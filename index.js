c49c14ae5113a20f5ae3e5bc404d12307ac068a5
Arieli Verza <arieliverza@gmail.com>
2022-10-26 19:13:25 -0300

    Correção funcao

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const lista = require('./tarefa1/usuarios-api/usuarios-api.json');
+const lista = require('./tarefa1/usuarios-api/users.json');
 
 //Responsável por criar a função de listar todos os usuários acima de 18 anos.
 
 const listarMaiores = () => {
-    return usuarios-api.filter(usuarios.idade) > 18
+    return users.filter((usuario) => usuario.idade) > 18
 }
-console.log(listarMaiores)
+listarMaiores()
