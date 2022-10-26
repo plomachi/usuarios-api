46317da56dff485d88977eb456ee0c99bf1c7b27
Arieli Verza <arieliverza@gmail.com>
2022-10-26 07:36:41 -0300

    Adicionando funcao ao arquivo index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const lista = require('./tarefa1/usuarios-api/usuarios-api.json');
+
+//Responsável por criar a função de listar todos os usuários acima de 18 anos.
+
+const listarMaiores = () => {
+    return usuarios-api.filter(usuarios.idade) > 18
+}
+console.log(listarMaiores)
